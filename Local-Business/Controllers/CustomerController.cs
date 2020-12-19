d5c8b6b9ac2e589127df4911a3d2b8f8094bd6c2
Himanshu Garg <garg0867@outlook.com>
2020-12-19 09:23:18 +0530

    commit sln

---
+++ Local-Business/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using Local_Business.Models.Customers;
+using Local_Business.Repo.Customers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Local_Business.Controllers
+{
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _ICustomerService;
+        public CustomerController(ICustomerService ICustomerService)
+        {
+            this._ICustomerService = ICustomerService;
+        }
+
+        [HttpPost]
+        [Route("registercustomer")]
+        public IActionResult RegisterCustomer([FromBody]Customer customer)
+        {
+            var result = _ICustomerService.RegisterCustomer(customer);
+            return this.Ok(result);
+        }
+    }
+}
